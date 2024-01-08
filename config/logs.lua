4fdcf4a20d2522ad7c89b3bf0dd6280f5ea36b16
Phillip Eis <45538799+PhillipEis@users.noreply.github.com>
2024-01-08 18:07:54 +0100

    fix logs

--- config/logs.lua
+++ config/logs.lua
@@ -1,24 +1,20 @@
 Logs = {}
-
--- Drug logs
-Logs["SoldDrugs"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-
 -- Zone logs
-Logs["CreateAlliance"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["RemoveAlliance"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["TransferZone"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["TakeZone"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
+Logs["CreateAlliance"] = ""
+Logs["RemoveAlliance"] = ""
+Logs["TransferZone"] = ""
+Logs["TakeZone"] = ""
 
 
 -- Reduce points
-Logs["ReducePoints"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
+Logs["ReducePoints"] = ""
 
 
 -- Admin logs
-Logs["OpenAdminZones"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["AdminCreateAlliance"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["AdminRemoveAlliance"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["AdminTransferZone"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["AdminResetZone"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["AdminSetPoint"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
-Logs["AdminToggleZone"] = "https://discord.com/api/webhooks/1192122897175679097/X1aKVlR28L4f4MJnbbxAMaQ-ZAI4336mBdQ7HXawaON9Xnake6QM6iQjoVbezP18RU1z"
+Logs["OpenAdminZones"] = ""
+Logs["AdminCreateAlliance"] = ""
+Logs["AdminRemoveAlliance"] = ""
+Logs["AdminTransferZone"] = ""
+Logs["AdminResetZone"] = ""
+Logs["AdminSetPoint"] = ""
+Logs["AdminToggleZone"] = ""
